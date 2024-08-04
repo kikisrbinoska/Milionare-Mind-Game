df3601fc1ef34aa51566e1c1cd39596070cf5f8a
kikisrbinoska <srbinoskakristina07@gmail.com>
2024-08-04 14:54:56 +0200

    Added Player class

---
+++ Milionare Mind Game/Player.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Milionare_Mind_Game
+{
+    public class Player
+    {
+        public string Name { get; set; }
+        public int Score { get; set; }
+
+
+        public Player(string name, int score)
+        {
+            Name = name;
+            Score = score;
+
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
