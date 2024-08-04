c8cadd301cbe6f55fb2b678a161690bb57550648
kikisrbinoska <srbinoskakristina07@gmail.com>
2024-08-04 15:46:12 +0200

    Added functionality for upload image for player

--- Milionare Mind Game/PlayerSetupForm.cs
+++ Milionare Mind Game/PlayerSetupForm.cs
@@ -13,6 +13,7 @@ namespace Milionare_Mind_Game
     public partial class PlayerSetupForm : Form
     {
         public string PlayerName { get; private set; }
+        public Image PlayerPhoto { get; private set; }
         public PlayerSetupForm()
         {
             InitializeComponent();
@@ -24,6 +25,18 @@ namespace Milionare_Mind_Game
             PlayerName = guna2TextBox1.Text;
             this.DialogResult = DialogResult.OK;
             this.Close();
+        }
+        private void guna2ButtonUploadPhoto_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Filter = "Image files (*.jpg;*.jpeg;*.png)|*.jpg;*.jpeg;*.png";
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    PlayerPhoto = Image.FromFile(openFileDialog.FileName);
+                    
+                }
+            }
         }
     }
 }
